bc8063f3d6b1a34385ca73e3ccdfe9c562c47305
Torben Meyer <torben.meyer@bakdata.com>
2020-11-25 20:17:20 +0100

    Remove k8s wrk

--- k8s/wrk/base.lua
+++
@@ -1,6 +0,0 @@
--- Select random key and request value from store (LB responsible for routing request)
-request = function()
-    local key = math.random(0, 1000000)
-    local path = "/store/" .. tostring(key)
-    return wrk.format(nil, path)
-end
